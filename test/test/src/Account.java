a6ed2b87253fadfd941e1e5fc048376893329e0a
김민창 <rlaalsckd5064@gmail.com>
2024-09-13 10:15:18 +0900

    get,set 작업

--- test/test/src/Account.java
+++ test/test/src/Account.java
@@ -6,4 +6,25 @@ public class Account {
     public String getAccountNo() {
         return accountNo;
     }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
 }
